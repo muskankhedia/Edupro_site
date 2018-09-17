9ceddb9c0e37de96905341792226f6efd6a80246
muskankhedia <muskan.khedia2000@gmail.com>
2018-09-18 01:27:43 +0530

    challenges page added and minor changes

--- app.js
+++ app.js
@@ -10,12 +10,17 @@ var signupRouter = require('./routes/signup');
 var menteeRouter = require('./routes/mentee');
 var mentorRouter = require('./routes/mentor');
 var resources = require('./routes/resources');
+var challenges = require('./routes/challenges');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// app.get('/',function(req,res){
+//   res.render('index');
+// });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,6 +34,7 @@ app.use('/signup',signupRouter);
 app.use('/mentee',menteeRouter);
 app.use('/mentor',mentorRouter);
 app.use('/resources',resources);
+app.use('/challenges',challenges);
 
 
 // catch 404 and forward to error handler
